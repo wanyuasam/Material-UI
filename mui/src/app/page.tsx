59a60bcb9476096e279e7bd58556551f95885435
wanyuasam <samuelwanyua@gmail.com>
2024-08-23 10:15:08 +0300

    Initial commit

---
+++ mui/src/app/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
+import { useRouter } from 'next/navigation'
+
+export default function Home() {
+    const router = useRouter()
+
+  return (
+    <main className={styles.main}>
+      <h1>MUI</h1>
+x      <Link href="/createPage">Create Page</Link>
+
+       <button onClick= {()=> router.push('/notes')}>Notes</button>
+
+
+    </main>
+  );
+}
